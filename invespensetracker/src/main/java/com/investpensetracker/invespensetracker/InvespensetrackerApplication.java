d35d5c96179ab5c14af41ad7c08f8472adfe5e34
DRK NET <naveenjeeva2105@gmail.com>
2024-09-07 13:28:00 +0000

    Commit after creating a springboot project

---
+++ invespensetracker/src/main/java/com/investpensetracker/invespensetracker/InvespensetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.investpensetracker.invespensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvespensetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvespensetrackerApplication.class, args);
+	}
+
+}
---
+++ invespensetracker/src/main/java/com/investpensetracker/invespensetracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.investpensetracker.invespensetracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InvespensetrackerApplication.class);
+	}
+
+}
